tmael/wolfssl

commit 97251bfd00c7059514348b5d2cd011f22ca7694a

Fix a couple of missing bounds checks found via code analyzer.

wolfcrypt/src/coding.c
@@ -297,8 +297,10 @@ static int CEscape(int escaped, byte e, byte* out, word32* i, word32 maxSz,
 
     if (raw)
         basic = e;
-    else
+    else if (e <= sizeof(base64Encode))
         basic = base64Encode[e];
+    else
+        return BAD_FUNC_ARG;
 
     /* check whether to escape. Only escape for EncodeEsc */
     if (escaped == WC_ESC_NL_ENC) {

src/conf.c
@@ -202,7 +202,10 @@ long wolfSSL_TXT_DB_write(WOLFSSL_BIO *out, WOLFSSL_TXT_DB *db)
                 return WOLFSSL_FAILURE;
             }
         }
-        idx[-1] = '\n';
+        if (idx > buf)
+            idx[-1] = '\n';
+        else
+            return WOLFSSL_FAILURE;
         sz = (int)(idx - buf);
 
         if (wolfSSL_BIO_write(out, buf, sz) != sz) {

commit b33a3606f48392e1e63e60322a2cbcf573df1580

wolfcrypt/src/coding.c: fix incorrect array bounds check in CEscape(), introduced in 97251bfd00 (before which there was no bounds check at all).

wolfcrypt/src/coding.c
@@ -297,7 +297,7 @@ static int CEscape(int escaped, byte e, byte* out, word32* i, word32 maxSz,
 
     if (raw)
         basic = e;
-    else if (e <= sizeof(base64Encode))
+    else if (e < sizeof(base64Encode))
         basic = base64Encode[e];
     else
         return BAD_FUNC_ARG;